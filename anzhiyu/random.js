173cd821a9e2625521e41bbb89ab302719a9daac
Zigao Wang <zigao.wang24@gmail.com>
2024-07-20 17:02:19 +0800

    Site updated: 2024-07-20 17:02:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/cpu-architectures/","2023/github-video/","2024/hello-world/","2024/internet-history/","2024/y6-eng-sci-review/","2024/trees-and-binary-trees/"];function toRandomPost(){
+var posts=["2024/cpu-architectures/","2023/github-video/","2024/hello-world/","2024/internet-history/","2024/y6-eng-sci-review/","2024/trees-and-binary-trees/","2024/AIGC/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
